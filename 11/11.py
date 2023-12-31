fb0f8f1c4c39ceb4e2faf562337478c6f6fe13b5
Aaron Estes <aaronestes@Aarons-MBP.attlocal.net>
2023-12-31 03:23:24 -0600

    find horizontal vertical and diagonal product of given matrix

--- 11/11.py
+++ 11/11.py
@@ -3,7 +3,6 @@
 ## up to X
 ## in the same direction (up, down, left, right, or diagonally) in the grid (N X N)?
 import numpy as np
-from itertools import zip_longest
 
 Y = 4
 X = 99
@@ -15,128 +14,81 @@ GREATEST_PRODUCT_DIAGONAL = 0
 
 path_to_board = '/Users/aaronestes/projects/project_euler/11/board.txt'
 
-def massage_data(f_read_split: list):
-    board = []
-    row = []
-    for num in f_read_split:
-        if len(row) < N - 1:
-            row.append(num)
-        elif len(row) == N - 1:
-            l = num.split('\n')
-            last_num_curr_row = l[0]
-            row.append(last_num_curr_row)
-            board.append(row)
-            row = []
-            if len(l) == 2:
-                first_num_next_row = l[1]
-                row.append(first_num_next_row)
-    return board
 
 def find_prod(l: list):
     p = np.prod(l)
     return p
 
-
-def find_greatest_product_horizontal(board) -> int:
-    greatest_product = 0
-    for row in board:
-        # print(f'row: {row}')
-        tmp_list = []
-        # print(f'tmp list {tmp_list}')
-        for num in row:
-            # print(f'int num {int(num)}')
-            if len(tmp_list) < 4:
-                tmp_list.append(int(num))
-            else:
-                ## reduce to get product
-                prod = np.prod(tmp_list)
-                if prod > greatest_product:
-                    # print(f'new greatest product {prod}')
-                    greatest_product = prod
-                ## keep scanning the row left to right
-                ## by forgetting the first element in our list
-                ## so that tmp_list is now sized at 3
-                tmp_list = tmp_list[1:]
-                tmp_list.append(int(num))
-                find_prod(tmp_list)
-            # print(f'tmp list after doing stuff {tmp_list}')
-    return greatest_product
-
-
 def transpose_board(board):
     return list(map(list, zip(*board)))
 
-def find_greatest_product_vertical(board) -> int:
-    ## transpose list of lists!
-    ## https://stackoverflow.com/questions/6473679/transpose-list-of-lists
-    board_transposed = transpose_board(board)
-    greatest_product = 0
-    for row in board_transposed:
-        # print(f'row: {row}')
-        tmp_list = []
-        # print(f'tmp list {tmp_list}')
-        for num in row:
-            # print(f'int num {int(num)}')
-            if len(tmp_list) < 4:
-                tmp_list.append(int(num))
-            else:
-                ## reduce to get product
-                prod = np.prod(tmp_list)
-                if prod > greatest_product:
-                    # print(f'new greatest product {prod}')
-                    greatest_product = prod
-                ## keep scanning the row left to right
-                ## by forgetting the first element in our list
-                ## so that tmp_list is now sized at 3
-                tmp_list = tmp_list[1:]
-                tmp_list.append(int(num))
-                find_prod(tmp_list)
-            # print(f'tmp list after doing stuff {tmp_list}')
-    GREATEST_PRODUCT_VERTICAL = greatest_product        
-    return greatest_product
+def get_max_row_prod(matrix: np.array) -> int:
+    max_row_prod = 0
+    for r in matrix:
+        prod = find_prod(r)
+        if prod > max_row_prod:
+            max_row_prod = prod
+    return max_row_prod
 
-def find_greatest_product_diagonal(board) -> int:
-    ## we need a better way to do this
-    greatest_product = 0
-    return greatest_product
+def get_max_col_prod(matrix: np.array) -> int:
+    max_col_prod = 0
+    for r in np.transpose(matrix):
+        prod = find_prod(r)
+        if prod > max_col_prod:
+            max_col_prod = prod
+    return max_col_prod
 
-def chunk(source, n):
-    return zip_longest(*([iter(source)] * n))
+def get_max_diag_prod(matrix: np.array):
+    diag_left_right = find_prod(matrix.diagonal())
+    diag_right_left = find_prod(np.rot90(matrix).diagonal())
+    return max(diag_left_right, diag_right_left)
 
-def better_way(s):
+def find_max_matrix_product(matrix: np.array) -> int:
+    max_row_prod = get_max_row_prod(matrix)
+    print(f'max_row_prod {max_row_prod}')
+    max_col_prod = get_max_col_prod(matrix)
+    print(f'max_col_prod {max_col_prod}')
+    max_diag_prod = get_max_diag_prod(matrix)
+    print(f'max_diag_prod {max_diag_prod}')
+    return max(max_row_prod, max_col_prod, max_diag_prod)
+
+def better_way(myS):
     ## take a Y x Y chunk out of the board
     ## do all vertical, horizontal, and diagonal products
     ## then move one index to the right until we get to the end of our row
-    ## then go to the new row
+    ## then go to the new row by returning to the first column and decrementing the row by 1
     ## until no rows left
-    return list(chunk(chunk(s.split(" "), Y), Y))
+    myL = myS.split()
+    myVector = []
+    myRow = []
+    count = 0
+    rowNum = 1
+    for num in myL:
+        count += 1
+        if rowNum == Y + 1:
+            break
+        if count <= 4:
+            myRow.append(int(num))
+        if len(myRow) == 4:
+            myVector.append(myRow)
+            myRow = []
+        if count == N:
+            count = 0
+            rowNum += 1
+
+    myMatrix = np.array(myVector)
+    print(f'myMatrix: {myMatrix}')
+    ## this is the max of our first matrix
+    ## but we need to find the max of the max
+    ## aka: the max of all the matrices
+    return find_max_matrix_product(myMatrix)
             
 
 def largest_product_in_a_grid():
     f = open(path_to_board, 'r')
     f_read = f.read()
-    f_read_split = f_read.split(" ")
-    board = massage_data(f_read_split)
-    # print(board)
-
-    ## this is good
-    # gph = find_greatest_product_horizontal(board)
-    # print(gph)
-
-    # ## this is good
-    # gpv = find_greatest_product_vertical(board)
-    # print(gpv)
-
-    ## TODO idk about this
-    # gpd = find_greatest_product_diagonal(board)
-    # print(gpd)
-
-    ## TODO better way
     bw = better_way(f_read)
     print(bw)
-
-
-
 
 if __name__ == '__main__':
     largest_product_in_a_grid()
