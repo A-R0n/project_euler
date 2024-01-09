df21a8354e53ecfcb0bd511d5381211bb6d4c489
Aaron Estes <aaronestes@Aarons-MBP.attlocal.net>
2024-01-09 03:43:58 -0600

    0.13 seconds with python 3.12

--- 30/Digit_Fifth_Powers.py
+++ 30/Digit_Fifth_Powers.py
@@ -5,16 +5,27 @@
 
 
 from time import perf_counter
+import math
+
 
 def get_upper_bound() -> int:
-    return 10000
+    ## how do we know this is the limit?
+    return 194979
 
-def digits_are_sum_fifth_power(num: int) -> bool:
-    return True
+def digits_are_sum_n_power(num: int, nPower) -> bool:
+    total = 0
+    for n in str(num):
+        total += math.pow(int(n), nPower)
+
+    if total == num:
+        print(f'total {total} eqauals num')
+        return True
+    return False
 
 def solve():
+    n_power = 5
     upper_bound = get_upper_bound()
-    return sum([num for num in range(upper_bound + 1) if digits_are_sum_fifth_power(num)])
+    return sum([num for num in range(2, upper_bound + 1) if digits_are_sum_n_power(num, n_power)])
 
 
 if __name__ == '__main__':
